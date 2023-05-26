762ee3820712ff373a0e70e33b7c890ccd1d44b7
와이즈라이트 <company@wiselight.kr>
2023-05-27 01:13:02 +0900

    feat: 학습된 모델 연동

--- app/app/src/main/java/kr/wiselight/svcm/CameraActivity.java
+++ app/app/src/main/java/kr/wiselight/svcm/CameraActivity.java
@@ -1,8 +1,11 @@
 package kr.wiselight.svcm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -10,15 +13,29 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
 import android.widget.TextView;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
 import org.jetbrains.annotations.NotNull;
+import org.pytorch.IValue;
+import org.pytorch.LiteModuleLoader;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -29,6 +46,7 @@ public class CameraActivity extends AppCompatActivity {
 
     private int REQUEST_CODE_PERMISSION = 101;
     private final String[] REQUIRED_PERMISSIONS = new String[] {"android.permission.CAMERA"};
+    private final String[] MODEL_CLASSES = new String[] {"7호선 4차분", "7호선 1차분", "7호선 2차분", "7호선 5차분", "7호선 3차분"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +58,7 @@ public class CameraActivity extends AppCompatActivity {
         if (!checkPermissions()) {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION);
         }
-
+        loadTorchModule("vehicle_1.pt");
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() -> {
             try {
@@ -61,11 +79,66 @@ public class CameraActivity extends AppCompatActivity {
         return true;
     }
 
+    Executor executor = Executors.newSingleThreadExecutor();
+
     void startCamera(@NotNull ProcessCameraProvider cameraProvider) {
         Preview preview = new Preview.Builder().build();
         CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder().setTargetResolution(new Size(224, 224))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(executor, image -> {
+            int rotation = image.getImageInfo().getRotationDegrees();
+            analyzeImage(image, rotation);
+            image.close();
+        });
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageAnalysis);
+    }
+
+    Module module;
+    void loadTorchModule(String fileName) {
+        File modelFile = new File(this.getFilesDir(), fileName);
+        try {
+            if (!modelFile.exists()) {
+                InputStream inputStream = getAssets().open(fileName);
+                FileOutputStream outputStream = new FileOutputStream(modelFile);
+                byte[] buffer = new byte[2048];
+                int bytesRead = -1;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+                inputStream.close();
+                outputStream.close();
+            }
+            module = LiteModuleLoader.load(modelFile.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void analyzeImage(ImageProxy image, int rotation) {
+        @SuppressLint("UnsafeOptInUsageError") Tensor inputTensor = TensorImageUtils.imageYUV420CenterCropToFloat32Tensor(image.getImage(), rotation, 224, 224,
+                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+        Tensor outputTensor = module.forward(IValue.from(inputTensor)).toTensor();
+        float[] scores = outputTensor.getDataAsFloatArray();
+        float maxScore = -Float.MAX_VALUE;
+        int maxScoreIdx = -1;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > maxScore) {
+                maxScore = scores[i];
+                maxScoreIdx = i;
+            }
+        }
+
+        String classResult = MODEL_CLASSES[maxScoreIdx];
+        Log.v("Torch", "Detected - " + classResult);
+
+        runOnUiThread(() -> {
+            textView.setText(classResult);
+        });
     }
 }
