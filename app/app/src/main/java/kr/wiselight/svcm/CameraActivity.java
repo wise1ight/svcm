fc0180c2c13864685bfae3f83e2bc26d84aca014
와이즈라이트 <company@wiselight.kr>
2023-05-27 00:37:45 +0900

    feat: CameraActivity에서 카메라 프로바이더 연동

--- app/app/src/main/java/kr/wiselight/svcm/CameraActivity.java
+++ app/app/src/main/java/kr/wiselight/svcm/CameraActivity.java
@@ -1,14 +1,71 @@
 package kr.wiselight.svcm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ExecutionException;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    PreviewView previewView;
+    TextView textView;
+
+    private int REQUEST_CODE_PERMISSION = 101;
+    private final String[] REQUIRED_PERMISSIONS = new String[] {"android.permission.CAMERA"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        previewView = findViewById(R.id.cameraView);
+        textView = findViewById(R.id.resultText);
+
+        if (!checkPermissions()) {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION);
+        }
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCamera(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                //erros
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private boolean checkPermissions() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void startCamera(@NotNull ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview);
     }
 }
